895b7a576753f745b28f70b1563cb4ce9cc9343b
Momen Alany <momen.sameer940@gmail.com>
2022-03-20 20:47:46 +0300

    converted the card to js

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,42 @@ function rectToPolar(div, x, y) {
 }
 
 // rectToPolar('plt', 3, 4)
+
+
+
+const cardDiv = document.getElementById("card-container-row");
+
+
+class Card {
+    constructor(title, desc, url) {
+        self.title = title;
+        self.desc = desc;
+        self.url = url;
+    }
+
+    render() {
+        let card = document.createElement('div');
+        card.innerHTML = ```
+        <div class="col">
+        <div class="card text-center">
+            <div class="card-body">
+                <h5 class="card-title">${self.title}</h5>
+                <p class="card-text">${self.desc}</p>
+                <a href="./tools/${self.url}.html" class="btn btn-primary">الدخول</a>
+            </div>
+        </div>
+        </div>
+        ```
+        cardDiv.appendChild(card)
+    }
+}
+
+
+var cardlist = [
+    new Card("test title", "watermelons", 'backup')
+];
+
+
+for (let card of cardlist) {
+    card.render()
+}
