4b21cb8682355a54a9fcc12337a5c99aad5fe5bd
Momen Alany <momen.sameer940@gmail.com>
2022-03-24 19:41:47 +0300

    fixed missing whitespace

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ class Card {
 
 
 var cardlist = [
-    new Card("Rectangular To Polar Coordinates", "تحويل الاحداثيات الكارتيزينيةالى احداثيات قطبية", 'rectToPolar'),
+    new Card("Rectangular To Polar Coordinates", "تحويل الاحداثيات الكارتيزينية الى احداثيات قطبية", 'rectToPolar'),
     new Card('Polar to Rectangular Coordinates', 'تحويل الاحداثيات القطبية الى احداثيات كارتيزينية', 'polarToRect'),
     new Card('Circular Motion Simulation', 'محاكاة واقعية للحركة الدائرية', 'circularMotion')
 ];
